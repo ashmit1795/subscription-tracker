2dd273b058c3512002966eeaed13195308471e80
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-16 20:03:11 +0530

    Uncomment bot categories for monitoring and link previews in Arcjet configuration

--- config/arcjet.js
+++ config/arcjet.js
@@ -16,8 +16,8 @@ const aj = arcjet({
 				"CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
 				// Uncomment to allow these other common bot categories
 				// See the full list at https://arcjet.com/bot-list
-				// "CATEGORY:MONITOR", // Uptime monitoring services
-				// "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+				"CATEGORY:MONITOR", // Uptime monitoring services
+				"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
                 "PostmanRuntime/7.44.0", // Postman user-agent header
                 "POSTMAN", // Postman
 			],
